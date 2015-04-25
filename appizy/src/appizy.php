b688b966bafb2f9fdb7556057041efc88f575653
Nicolas Hefti <nicolas.hefti@gmail.com>
2015-04-25 16:39:14 +0200

    Add error handling for PHP memory exhausted

--- appizy/src/appizy.php
+++ appizy/src/appizy.php
@@ -1,7 +1,9 @@
 <?php
 
-$filePath = $argv[1];
+// Register a shutdown function to catch FATAL errors
+register_shutdown_function('fatalHandler');
 
+$filePath = $argv[1];
 
 if (!is_file($filePath)) {
     echo("File not found: $filePath. \n");
@@ -22,20 +24,31 @@ $xml_path[] = $extractDir . "/content.xml";
 include('Tool.php');
 
 $tool = new Tool(true);
-$tool->tool_parse_wb($xml_path);
-$tool->tool_clean();
 
-$elements = $tool->tool_render(null, 1, array(
-    'compact css'  => false,
-    'jquery tab'   => false,
-    /* 'freeze' => $option_freeze,*/
-    'print header' => true,
-));
+try {
+    echo "Parsing spreadsheet \n";
+    $tool->tool_parse_wb($xml_path);
+} catch (Exception $e) {
+    echo 'Error while parsing spreadsheet: ' . $e->getMessage();
+}
+
+try {
+    echo 'Rendering application';
+    $tool->tool_clean();
+
+    $elements = $tool->tool_render(null, 1, array(
+        'compact css'  => false,
+        'jquery tab'   => false,
+        /* 'freeze' => $option_freeze,*/
+        'print header' => true,
+    ));
+} catch (Exception $e) {
+    echo 'Error while rendering the webapplication: ' . $e->getMessage();
+}
 
 $htmlTable = $elements['content'];
 
 // Import variables in local
-
 extract($elements, EXTR_OVERWRITE);
 
 // Start output buffering
@@ -55,7 +68,6 @@ fclose($open);
 // Removes temporary file
 delTree($extractDir);
 
-
 function delTree($dir)
 {
     $files = array_diff(scandir($dir), array('.', '..'));
@@ -64,4 +76,20 @@ function delTree($dir)
     }
 
     return rmdir($dir);
+}
+
+
+function fatalHandler()
+{
+    $error = error_get_last();
+
+    if ($error !== null) {
+        $message = $error["message"];
+
+        if (preg_match('/Allowed memory size of (\\d+) bytes exhausted/', $message)) {
+            echo "X File too big. Have look at the FAQ for more information. \n";
+        } else {
+            echo "X Unknown fatal error. Please report to hello@appizy.com \n";
+        }
+    }
 }
